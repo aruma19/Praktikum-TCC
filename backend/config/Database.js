ea859d9447aea75372ba8ef1ace136ab0ebba0fd
aruma19 <muhammadalmasfarrosd@gmail.com>
2025-03-25 15:31:40 +0700

    Mengubah file Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('crud_notes', 'root', '', {
+const db = new Sequelize('crud_notes', 'root', 'almasfarros19', {
     host: '34.171.75.159',
     dialect: 'mysql'
 });
