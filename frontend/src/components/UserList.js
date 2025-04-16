a77617ad7f6b2248f4cf76b60a0e1a489647bfe8
aruma19 <muhammadalmasfarrosd@gmail.com>
2025-04-16 19:02:23 +0700

    Menambahkan File baru

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
-import { BASE_URL } from "./utils";
+import { BASE_URL } from "../utils";
 
 const UserList = () => {
     const [notes, setNotes] = useState([]);
