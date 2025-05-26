5b0825006751179b78f817832320d6908640ba32
aruma19 <muhammadalmasfarrosd@gmail.com>
2025-05-26 16:49:22 +0700

    Menperbaiki isi file index.js  be

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,8 @@ app.use(UserRoute);
     await db.sync(); // Atau db.sync({ force: true }) jika ingin reset tabel
     console.log("Database synced!");
     
-    app.listen(5002, () => console.log("Server connected on port 5002"));
+    const PORT = process.env.PORT || 5002;
+    app.listen(PORT, () => console.log(`Server connected on port ${PORT}`));
   } catch (error) {
     console.error("DB Sync Error:", error);
   }
