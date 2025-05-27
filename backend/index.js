069d8dbf064d7d3b60004cc0c34c0e701bb69cc8
aruma19 <muhammadalmasfarrosd@gmail.com>
2025-05-27 16:13:44 +0700

    ganti link frontend local ke link benera

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.set("view engine", "ejs");
 dotenv.config();
 
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://national-news-dot-f-08-450706.uc.r.appspot.com/" }));
 app.use(express.json());
 
 app.get("/", (req, res) => res.render("index"));
